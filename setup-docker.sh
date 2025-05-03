2c1257d461da56d572dd0db986a28c37bee37f3e
Gravita <12893402+gravit0@users.noreply.github.com>
2025-05-04 00:12:29 +0700

    Upgrade to 5.6.13

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,16 +1,9 @@
 #!/bin/bash -e
 set -e
-BRANCH=v5.6.11
-RUNTIME_BRANCH=v4.0.6
+BRANCH=v5.6.13
+RUNTIME_BRANCH=v4.0.7
 
 echo -e "\033[32mPhase 0: \033[33mChecking\033[m";
-
-which java || ( echo -e "\033[31mCheck failed: java not found. Please install JDK 21: \033[32https://gravitlauncher.com/install\033[m" && exit 1 );
-which javac || ( echo -e "\033[31mCheck failed: javac not found. Please install JDK 21: \033[32https://gravitlauncher.com/install\033[m" && exit 1 );
-which git || ( echo -e "\033[31mCheck failed: git not found. Please install git\033[m" && exit 1 );
-#which curl || ( echo -e "\033[31mCheck failed: curl not found. Please install curl\033[m" && exit 1 );
-
-(javac -version | grep " 21") || ( echo -e "\033[31mCheck failed: javac version unknown. Supported Java 21+. Please install JDK 21: \033[32https://gravitlauncher.com/install\033[m" && exit 1 );
 
 echo -e "\033[32mLauncher branch: \033[33m$BRANCH\033[m"
 
@@ -23,7 +16,6 @@ git submodule update --init --recursive;
 echo -e "\033[32mPhase 2: \033[33mBuild\033[m";
 ./gradlew -Dorg.gradle.daemon=false assemble || ( echo -e "\033[31mBuild failed. Stopping\033[m" && exit 100 );
 cd ..;
-mkdir libraries;
 mkdir launcher-libraries;
 mkdir launcher-libraries-compile;
 echo -e "\033[32mPhase 3: \033[33mClone runtime repository\033[m";
@@ -33,9 +25,12 @@ cd srcRuntime;
 cd ..;
 echo -e "\033[32mPhase 4: \033[33mCopy files\033[m";
 cp src/LaunchServer/build/libs/LaunchServer.jar .;
-cp -r src/LaunchServer/build/libs/libraries ./libraries/default;
+cp -r src/LaunchServer/build/libs/libraries ./libraries;
+cp -r src/LaunchServer/build/libs/proguard ./proguard;
 cp -r src/LaunchServer/build/libs/launcher-libraries ./launcher-libraries/default;
 cp -r src/LaunchServer/build/libs/launcher-libraries-compile ./launcher-libraries-compile/default || true;
+cp -r src/javaargs.txt javaargs.txt
+cp -r src/java24args.txt java24args.txt
 mkdir modules;
 cp -r srcRuntime/runtime .;
 mkdir launcher-modules;
@@ -48,7 +43,6 @@ cp -r src/modules/*_module/build/libs/*_module.jar compat/launchserver-modules
 mkdir compat/launcher-modules
 cp -r src/modules/*_lmodule/build/libs/*_lmodule.jar compat/launcher-modules
 mkdir data
-ln -s ../data/libraries libraries/custom
 ln -s ../data/launcher-libraries launcher-libraries/custom
 ln -s ../data/modules modules/custom
 ln -s ../data/launcher-modules launcher-modules/custom
@@ -88,7 +82,6 @@ chmod +x install_launcher_module.sh
 
 cat <<EOF > start.sh
 #!/bin/bash
-mkdir -p libraries
 mkdir -p launcher-libraries
 mkdir -p modules
 mkdir -p launcher-modules
@@ -106,7 +99,7 @@ then
 else
     cp -r ../runtime runtime
 fi
-exec java -Xmx512M -Dlaunchserver.dir.libraries=../libraries -Dlaunchserver.dir.launcher-libraries=../launcher-libraries -Dlaunchserver.dir.modules=../modules -Dlaunchserver.dir.launcher-modules=../launcher-modules -Dlauncher.useSlf4j=true -jar ../LaunchServer.jar \$@
+exec java -Xmx512M -Dlaunchserver.dir.libraries=../libraries -Dlaunchserver.dir.launcher-libraries=../launcher-libraries -Dlaunchserver.dir.modules=../modules -Dlaunchserver.dir.launcher-modules=../launcher-modules -Dlaunchserver.dir.proguard-libraries=../proguard @/app/javaargs.txt @/app/java24args.txt --module-path /app/LaunchServer.jar:/app/libraries --add-modules ALL-MODULE-PATH -m launchserver/pro.gravit.launchserver.LaunchServerStarter \$@
 EOF
 chmod +x start.sh
 echo -e "\033[32mPhase 4: \033[33mDelete source files\033[m";
